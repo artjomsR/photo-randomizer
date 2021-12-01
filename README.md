47e453b50f9f628b91ea7bfac362517b3b04ea39
Art Rizihs <artjoms.rizihs@gmail.com>
2021-12-01 11:02:42 +0000

    added support for flat folder

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # photo-randomizer
 
-A script I wrote after discovering that my digital photo frame has a limit to how many photos it can display. This creates a copy of a photo library and randomly deletes a percentage of all photos in all subfolders - the resulting directory should be a near perfect fit for copying to the photo frame storage device.
+A script I wrote after discovering that my digital photo frame has a limit to how many photos it can display. It creates a copy of a photo library and randomly deletes a percentage of all photos in all subfolders - the resulting directory should be a near perfect fit for copying to the photo frame storage device.
 
-Needs Python 3. To run, edit the file first and set the `variables maximum_no_of_files`, `original_dir` and `walk_dir`. Then simply run the file.
+Needs Python 3. To run, edit the file `.py` file first to set the variables at the top. If the files in the photo library are not separated into subfolders, then `folder_separator` string will be used to put files into their respective folders.
--- select subset photos for photoframe.py
+++ select subset photos for photoframe.py
@@ -1,34 +1,52 @@
 import os, sys, glob, random, distutils, shutil
 from pathlib import Path
-# import numpy as np
+from typing import DefaultDict
 
-def copy_and_overwrite(from_path, to_path):
+maximum_no_of_files = 1735
+minimum_number_of_files_to_be_trimmed = 4
+original_dir = 'E:\JPG Export\Photo Frame\Sorted'
+trim_dir = 'E:/PHOTOFRAME_PHOTOS_DELETE_ME'
+folder_separator = '-- '
+
+def copy_folder_with_overwrite(from_path, to_path):
     if os.path.exists(to_path):
         shutil.rmtree(to_path)
     print(f'copying {from_path} to {to_path}...')
     shutil.copytree(from_path, to_path)
 
+def folder_structure_is_flat(path):
+	return len(os.listdir(path)) > maximum_no_of_files
 
-maximum_no_of_files = 1735
-original_dir = 'D:\JPG Export\Photo Frame\Sorted'
-walk_dir = 'D:\PHOTOFRAME_PHOTOS_DELETE_ME'
+def put_files_in_subfolders(source_dir_path):
+	for combined_filename in os.listdir(source_dir_path):
+		basename, extension = os.path.splitext(combined_filename)
+		parent_folder = basename.split(folder_separator)
+		
+		path_to_parent_folder = f'{source_dir_path}\{parent_folder[0]}';
+		if not os.path.exists(path_to_parent_folder):
+			os.makedirs(path_to_parent_folder)
+		shutil.move(f'{source_dir_path}\{combined_filename}', f'{path_to_parent_folder}\{combined_filename}')
+
 
 if not (os.path.isdir(original_dir)):
 	input(f'{original_dir} does not exist, aborting...')
 	sys.exit()
 
-copy_and_overwrite(original_dir, walk_dir)
+copy_folder_with_overwrite(original_dir, trim_dir)
 
-total_files = len(list(Path(walk_dir).rglob("*.jpg")))
+if folder_structure_is_flat(trim_dir):
+	put_files_in_subfolders(trim_dir)
+
+total_files = len(list(Path(trim_dir).rglob("*.jpg")))
 deletion_coefficient = maximum_no_of_files / total_files
 print(f'Total files:{total_files}, deletion_coefficient = {deletion_coefficient}')
 
-for root, subdirs, files in os.walk(walk_dir):
+for root, subdirs, files in os.walk(trim_dir):
 	for subdir in subdirs:
 		print(os.path.join(root, subdir))
 		os.chdir(os.path.join(root, subdir))
 		folder_jpgs = glob.glob("*.jpg")
-		if len(folder_jpgs) > 4:
+		if len(folder_jpgs) > minimum_number_of_files_to_be_trimmed:
 			no_of_files_to_delete = len(folder_jpgs) - int(len(folder_jpgs) * deletion_coefficient)
 			print(f'files:{len(folder_jpgs)}, files to delete:{no_of_files_to_delete}')
 			for file in random.sample(folder_jpgs, no_of_files_to_delete):
